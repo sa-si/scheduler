e73e1b1dcd21b053509fb84aabe2dd0019125f97
sa-si <sancho6351@gmail.com>
2022-09-24 16:09:03 +0900

    認可設定

--- routes/web.php
+++ routes/web.php
@@ -21,34 +21,36 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
-Route::get('/weekly-calendar', function () {
-    return view('weekly-calendar');
-});
-Route::get('/monthly-calendar', function () {
-    return view('monthly-calendar');
-});
-Route::get('/year-calendar', function () {
-    return view('year-calendar');
-});
-Route::get('/planning-task-input', function () {
-    return view('planning-task-input');
-});
-Route::get('/execution-task-input', function () {
-    return view('execution-task-input');
-});
-Route::get('/planning-task-update', function () {
-    return view('planning-task-update');
-});
-Route::get('/execution-task-update', function () {
-    return view('execution-task-update');
-});
-Route::get('/trash-can', function () {
-    return view('trash-can');
-});
-Route::get('/analysis', function () {
-    return view('analysis');
-});
-Route::get('/profile', function () {
-    return view('profile');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+    Route::get('/weekly-calendar', function () {
+        return view('weekly-calendar');
+    });
+    Route::get('/monthly-calendar', function () {
+        return view('monthly-calendar');
+    });
+    Route::get('/year-calendar', function () {
+        return view('year-calendar');
+    });
+    Route::get('/planning-task-input', function () {
+        return view('planning-task-input');
+    });
+    Route::get('/execution-task-input', function () {
+        return view('execution-task-input');
+    });
+    Route::get('/planning-task-update', function () {
+        return view('planning-task-update');
+    });
+    Route::get('/execution-task-update', function () {
+        return view('execution-task-update');
+    });
+    Route::get('/trash-can', function () {
+        return view('trash-can');
+    });
+    Route::get('/analysis', function () {
+        return view('analysis');
+    });
+    Route::get('/profile', function () {
+        return view('profile');
+    });
 });
